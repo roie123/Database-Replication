53d4bda212bb4bf0d06f9c4589cbd0977c00befe
roie123 <roieivri123@gmail.com>
2024-09-15 12:47:10 +0300

    first

---
+++ src/main/java/com/System_Design/_Alex_Xu/Database/Replication/DatabaseReplicationApplication.java
@@ -0,0 +1,13 @@
+package com.System_Design._Alex_Xu.Database.Replication;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DatabaseReplicationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DatabaseReplicationApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/System_Design/_Alex_Xu/Database/Replication/DatabaseReplicationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.System_Design._Alex_Xu.Database.Replication;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DatabaseReplicationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
